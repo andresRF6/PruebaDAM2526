b049c2d67d276e3095dc556a76e07ec855d2cd13
achillon <andres.chillonsesma@educa.madrid.org>
2025-11-12 13:52:28 +0100

    v3 con proyecto

---
+++ ARRAYS_FINAL_MONSTER/src/arrays_final_monster/ArraysExample1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arrays_final_monster;
+
+/**
+ *
+ * @author andres.chillon
+ */
+public class ArraysExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+
+        for (int numero : numeros) {
+            System.out.println("Numero en array:"+numero);
+        }
+        
+        
+    }
+
+}
